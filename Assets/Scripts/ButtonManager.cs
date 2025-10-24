fed89947487c41bb21f66e0f6c0b06d9c24c33e3
Frosti-Dev <bennyfranken3@gmail.com>
2025-10-24 15:56:13 -0300

    Basic Main Menu Added
    Moves from Main Menu to Options Menu
    Moves from Options to Main Menu
    Quit Button
    Play Moves to Game Scene

---
+++ Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour
+{
+  public void OnPlayButton()
+    {
+        //Load Game Scene (Change to whatever the Game Scene is)
+        SceneManager.LoadScene(2);
+    }
+
+    public void OnOptionButton()
+    {
+        //Load Options Scene (Change to whatever options scene is)
+        SceneManager.LoadScene(1);
+    }
+
+    public void OnExitButton()
+    {
+        //Leaves Game!
+        Application.Quit();
+    }
+
+    public void OnBackButton()
+    {
+        //From Options back to Main Menu
+        SceneManager.LoadScene(0);
+    }
+
+}
