1d6032d0b457221fa7ccc331f41ce71ac8f68e2e
Frosti-Dev <bennyfranken3@gmail.com>
2025-10-25 16:57:51 -0300

    Main Menu Switch
    The main menu uses panels instead of switching between scenes.

--- Assets/BenScripts/ButtonManager.cs
+++ Assets/BenScripts/ButtonManager.cs
@@ -3,17 +3,34 @@ using UnityEngine.SceneManagement;
 
 public class ButtonManager : MonoBehaviour
 {
+    public GameObject optionsMenu;//panel for options
+    public GameObject creditsMenu;//panel for credits
+    public GameObject controlsMenu;//panel for controls
 
-  public void OnPlayButton()
+
+    private void Start()
+    {
+        ClearMenus();
+    }
+
+    public void ClearMenus()
+    {
+        optionsMenu.SetActive(false);
+        creditsMenu.SetActive(false);
+        controlsMenu.SetActive(false);
+    }
+
+    public void OnPlayButton()
     {
         //Load Game Scene (Change to whatever the Game Scene is)
-        SceneManager.LoadScene(3);
+        SceneManager.LoadScene(1);
     }
 
     public void OnOptionButton()
     {
-        //Load Options Scene (Change to whatever options scene is)
-        SceneManager.LoadScene(1);
+        //Opens Options Panel
+        ClearMenus();
+        optionsMenu.SetActive(true);
     }
 
     public void OnExitButton()
@@ -24,14 +41,22 @@ public class ButtonManager : MonoBehaviour
 
     public void OnCreditsButton()
     {
-        //Opens Credits Scene (Change to whatever the credits scene is)
-        SceneManager.LoadScene(2);
+        //Opens Credits Panel 
+        ClearMenus();
+        creditsMenu.SetActive(true);
+    }
+
+    public void OnControlsButton()
+    {
+        //Opens Controls Panel
+        ClearMenus();
+        controlsMenu.SetActive(true);
     }
 
     public void OnBackButton()
     {
-        //From Options back to Main Menu
-        SceneManager.LoadScene(0);
+        //Close panel
+        ClearMenus();
     }
 
     public void OnProgressButton()
@@ -43,12 +68,12 @@ public class ButtonManager : MonoBehaviour
 
         if (sceneName == "Game(Day)")
         {
-            SceneManager.LoadScene(4);
+            SceneManager.LoadScene(2);
         }
 
         else
         {
-            SceneManager.LoadScene(3);
+            SceneManager.LoadScene(1);
         }
         
     }
